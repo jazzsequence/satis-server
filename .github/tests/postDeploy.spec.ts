d340d9519b337fdde36bae80762e920b5194c585
Chris Reynolds <chris@jazzsequence.com>
2025-08-01 09:56:38 -0600

    fix playwright tests

--- .github/tests/postDeploy.spec.ts
+++ .github/tests/postDeploy.spec.ts
@@ -5,9 +5,14 @@ const BASE_URL = process.env.BASE_URL || 'https://dev-cxr-satis-server.pantheons
 test.describe('Does the site still work?', () => {
   test('Homepage renders without error', async ({ page }) => {
     const res = await page.goto(BASE_URL);
+    if (res?.status() !== 200) {
+        console.error('Non-200 response:', res?.status());
+        console.error('Body:', await page.content());
+    }
+
     expect(res?.status()).toBe(200);
     const content = await page.content();
-	expect(content).not.toMatch(/Fatal error|Warning:|Notice:/i);
+	expect(content).not.toMatch(/^(Fatal error|Warning:|Notice:)/im);
     await expect(page.locator('body')).toBeVisible();
   });
 
@@ -17,16 +22,16 @@ test.describe('Does the site still work?', () => {
     await page.waitForLoadState('networkidle');
     await expect(page.locator('h1')).toContainText(/jazzsequence\/satis-server/i);
 	const content = await page.content();
-	expect(content).not.toMatch(/Fatal error|Warning:|Notice:/i);
+	expect(content).not.toMatch(/^(Fatal error|Warning:|Notice:)/im);
   });
 
   test('fair/fair-plugin loads', async ({ page }) => {
     const res = await page.goto(`${BASE_URL}/#fair/fair-plugin`);
     expect(res?.status()).toBe(200);
     await page.waitForLoadState('networkidle');
-    await expect(page.locator('div#fair/fair-plugin')).toContainText(/fair\/fair-plugin/i);
+    await expect(page.locator('body')).toContainText(/fair\/fair-plugin/i);
 	const content = await page.content();
-	expect(content).not.toMatch(/Fatal error|Warning:|Notice:/i);
+	expect(content).not.toMatch(/^(Fatal error|Warning:|Notice:)/im);
   });
 
   test('Headers expected', async ({ page }) => {
